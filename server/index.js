2a1148987e678b158e8f8bf2c7398bb01803366e
Vishaal Venkatesan <vishaalvo3@gmail.com>
2024-06-01 22:14:30 +0530

    changed logic for backend and file structure to

--- server/index.js
+++ server/index.js
@@ -1,33 +1,33 @@
-// backend/index.js
 const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
-import process from 'process';
-const app = express();
-const PORT = process.env.PORT || 4000;
 
-// CORS Middleware
-app.use(cors({
-  origin: process.env.FRONTEND_URL, // Use environment variable for frontend URL
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+const app = express();
+const PORT = process.env.PORT || 4001;
+
+// Apply CORS middleware
+app.use(cors());
 
 app.get('/api/drivers', async (req, res) => {
   const { country } = req.query;
 
+  // Validate country parameter
   if (!country || !['USA', 'IND'].includes(country)) {
     return res.status(400).send({ error: 'Invalid country code' });
   }
 
   try {
+    // Fetch data from external API based on country
     const response = await axios.get(`https://us-central1-projectexperiment-420611.cloudfunctions.net/assignApi?countryName=${country}`);
     res.send(response.data);
   } catch (error) {
+    // Handle errors
+    console.error('Error fetching data from API:', error.message);
     res.status(500).send({ error: 'Error fetching data from API' });
   }
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on PORT ${PORT}`);
 });
+
